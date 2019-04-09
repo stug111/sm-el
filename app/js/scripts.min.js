010dc68853b1c19070434de442ee7e61bd6d9d4b
Vadim Mishkhozhev <vadim.stug@gmail.com>
2019-04-09 22:25:07 +0700

    Добавлен футер

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -1,5 +1,24 @@
 $(function() {
+	var wrapper = $(".file_upload"),
+		inp = wrapper.find("input"),
+		btn = wrapper.find("span");
 
-	// Custom JS
+	var file_api =
+		window.File && window.FileReader && window.FileList && window.Blob
+			? true
+			: false;
 
+	inp
+		.change(function() {
+			var file_name;
+			if (file_api && inp[0].files[0]) file_name = inp[0].files[0].name;
+			else file_name = inp.val().replace("C:\\fakepath\\", "");
+
+			if (!file_name.length) return;
+
+			if (btn.is(":visible")) {
+				btn.text(file_name);
+			} else btn.text(file_name);
+		})
+		.change();
 });
